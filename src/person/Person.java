09ce8daa0c085697659dc50266a0cebe59f5d2f4
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2020-02-24 18:19:33 +0530

    constructor defined

---
+++ src/person/Person.java
@@ -0,0 +1,15 @@
+package person;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LinkedList<String> contacts;
+    private String email;
+
+    public Person(String firstName, String lastName, LinkedList<String> contacts, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contacts = contacts;
+        this.email = email;
+    }
+}
