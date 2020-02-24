6a0a0c5deefb36c63e95ef845f3870ad5a06fc50
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2020-02-24 18:40:12 +0530

    defining a constructor of linked list class

--- src/task/Tasks.java
+++ src/task/Tasks.java
@@ -1,7 +1,9 @@
 package task;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Tasks {
     Scanner scanner = new Scanner(System.in);
+    LinkedList<String> linkedList = new LinkedList<>();
 }
