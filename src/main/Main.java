102888bb0bd85f14072670774dddb6ab627b4297
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2020-02-24 18:44:11 +0530

    object call

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import linkedList1.LinkedList1;
+import task.Tasks;
 
 import java.util.Scanner;
 
@@ -8,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LinkedList1 linkedList1 = new LinkedList1();
+        Tasks tasks = new Tasks();
     }
 }
